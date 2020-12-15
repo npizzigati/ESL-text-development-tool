5441f73cb9a216eed3b5df08f0606ccdede7fcc7
Nicholas Pizzigati <npizzigati@gmail.com>
2020-12-14 18:58:49 -0600

    Implement basic search for main window

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,20 +1,39 @@
+'use strict';
+
 const trixElement = document.querySelector("trix-editor")
 const trixEditor = trixElement.editor
 const wordSeparators = [' ', '.', '!', '?', '-', ':', ';', 'Enter'];
 let wordStart;
 let wordEnd;
 let word;
-let trixContent;
 let wordInProgress = false;
+let trixSearchUnderway = false;
 
-trixElement.addEventListener("keydown", event => {
+// Add Trix format for highlighting search matches
+Trix.config.textAttributes.searchHighlight = {
+  style: { backgroundColor: "moccasin" },
+  parser: function(element) {
+    return element.style.backgroundColor === "moccasin"
+  },
+  inheritable: true
+};
+
+trixEditor.insertString('A highlighted part');
+trixEditor.setSelectedRange([2, 13]);
+trixEditor.activateAttribute('searchHighlight');
+trixEditor.setSelectedRange([0, 0]);
+
+trixElement.addEventListener('keydown', event => {
+  // Also need to account for other non letter keys, like Windows
+  // key, which do not start a word.
   if (!wordInProgress && !isSeparator(event.key)) {
-    wordInProgress = true;
     console.log('word in progress');
-    wordStart = trixEditor.getPosition();
+    wordInProgress = true;
+    wordStart = trixEditor.getSelectedRange()[0];
   } else if (isSeparator(event.key)) {
+    console.log('word not in progress');
     wordInProgress = false;
-    wordEnd = trixEditor.getPosition();
+    wordEnd = trixEditor.getSelectedRange()[0];
     console.log(`wordStart: ${wordStart}, wordEnd: ${wordEnd}`);
     word = retrieveWord(wordStart, wordEnd); 
     console.log(`word:..${word}..`);
@@ -24,6 +43,30 @@ trixElement.addEventListener("keydown", event => {
   }
 }, false);
 
+const searchBox = document.getElementById('searchBox');
+const searchDownButton = document.getElementById('searchDown');
+
+searchBox.addEventListener('keyup', event => {
+  let searcher = new Searcher(searchBox.value);
+  searcher.execute();
+}, false);
+
+function Searcher(searchString) {
+  this.fullText = trixEditor.getDocument().toString();
+  this.searchString = searchString;
+  this.matches = [];
+  this.execute = function() {
+    clearSearchHighlighting(this.fullText);
+    this.matches = findMatches(this.fullText, this.searchString); 
+    console.log(this.matches);
+    if (this.matches.length > 0) {
+      highlightMatch(this.matches[0], this.searchString.length);
+      scrollToMatch(this.matches[0]);
+      searchBox.focus();
+    }
+  }
+} 
+
 function isSeparator(key) {
   return wordSeparators.includes(key);
 }
@@ -32,13 +75,49 @@ function isInList(word) {
   return Object.keys(inflections_map).includes(word);
 }
 
+// TODO: This shouldn't be searching for regex (e.g. a period for
+// any character)
+function findMatches(text, searchString,
+                     startIndex = 0) {
+  let fragment = text.slice(startIndex);
+  let fragmentMatchIndex = fragment.search(searchString);
+  if (fragmentMatchIndex == -1 || searchString == '') {
+    return [];
+  }
+  let fullTextMatchIndex = fragmentMatchIndex + startIndex;
+  startIndex = fullTextMatchIndex + 1
+  return [fullTextMatchIndex].concat(findMatches(text,
+                                         searchString,
+                                         startIndex));
+}
+
+function clearSearchHighlighting(fullText) {
+  trixEditor.setSelectedRange([0, fullText.length]);
+  trixEditor.deactivateAttribute('searchHighlight');
+  trixEditor.setSelectedRange([0, 0]);
+  searchBox.focus();
+}
+
+function highlightMatch(startIndex, length) {
+  let endIndex = startIndex + length;
+  trixEditor.setSelectedRange([startIndex, endIndex]);
+  trixEditor.activateAttribute('searchHighlight');
+  trixEditor.setSelectedRange([startIndex, startIndex]);
+}
+
+function scrollToMatch(startIndex) {
+  console.log('In scrollToMatch function');
+  let highlightedElement = document.querySelector('div span[style ^= "background-color"]')
+  highlightedElement.scrollIntoView({behavior: "smooth",
+                                     block: "center"});
+}
+
 function retrieveWord(wordStart, wordEnd) {
   let fullText = trixEditor.getDocument().toString();
   return fullText.slice(wordStart, wordEnd);
 }
 
-// when making selection, it's necessary to add 1 to end position
-// (selection ends just before the start of that last index)
+// selection ends just before the start of last index
 function makeBold(startIndex, endIndex) {
   trixEditor.setSelectedRange([startIndex,
                                endIndex]);
@@ -49,3 +128,4 @@ function makeBold(startIndex, endIndex) {
                                endIndex]);
   trixEditor.deactivateAttribute('bold');
 }
+
